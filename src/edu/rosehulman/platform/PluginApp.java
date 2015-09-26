b4a996678f5c92a112bd085ee8d1da55ac48658e
Austin Willis <willisaj@rose-hulman.edu>
2015-09-26 17:01:07 -0400

    Finishing status module

--- src/edu/rosehulman/platform/PluginApp.java
+++ src/edu/rosehulman/platform/PluginApp.java
@@ -2,22 +2,49 @@ package edu.rosehulman.platform;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+
+import edu.rosehulman.gui.StatusModule;
 
 public class PluginApp extends JFrame {
 	
+	private StatusModule statusModule;
+	
 	public PluginApp() {
+		super("Super Awesome Application for Running Plugins");
+		
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		Button b = new Button("Listing");
-		this.add(b, BorderLayout.EAST);
+		this.setSize(1440, 810);
+
+		Button execution = new Button("execution");
+		this.add(execution, BorderLayout.CENTER);
+		Button listing = new Button("Listing");
+		this.add(listing, BorderLayout.WEST);
+		
+		JPanel southPanel = new JPanel();
+		southPanel.setLayout(new BorderLayout());
+		
+		//Status Module
+		this.statusModule = new StatusModule(1220, 610);
+		southPanel.add(statusModule, BorderLayout.CENTER);
+		
+		Button controlModule = new Button("Control MOdule");
+		controlModule.setPreferredSize(new Dimension(200, 200));
+		controlModule.setSize(new Dimension(200, 200));		
+		southPanel.add(controlModule, BorderLayout.WEST);
+		this.add(southPanel, BorderLayout.SOUTH);
 	}
 	
 	public static void main(String[] args) {
 		PluginApp app = new PluginApp();
-		app.show();
+		app.setVisible(true);
 	}
 
 }
